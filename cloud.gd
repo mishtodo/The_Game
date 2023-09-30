8a66d2d65816b6153dfa59c0fd7d652a1ba2eb74
k0ndratov <peckondratov@gmail.com>
2023-09-30 19:31:36 +0500

    del unnesesary method

--- cloud.gd
+++ cloud.gd
@@ -5,7 +5,3 @@ func _ready():
 	var sprite = get_node("Cloud2")
 	var flip_h = get_meta("flip_h", false)
 	if (flip_h): sprite.flip_h = true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
