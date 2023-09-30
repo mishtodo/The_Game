c0893d473a5f5029719279e9b57f30bd658d7eea
mishtodo <todomikha@gmail.com>
2023-09-30 18:40:59 +0300

    added jumping on the rain clouds

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,11 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite = get_node("Sprite2D")
+@onready var rain_cloud_check = get_node("rain_cloud_check")
+
+func is_on_rain_cloud():
+	if not rain_cloud_check.is_colliding():
+		return false
+	var collider = rain_cloud_check.get_collider()
+	if collider is rain_cloud:
+		return true
+	else:
+		return false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,7 +26,10 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		# Rain cloud jump.
+		if is_on_rain_cloud():
+			velocity.y += JUMP_VELOCITY
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
--- cloud.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var sprite = get_node("Cloud2")
-	var flip_h = get_meta("flip_h", false)
-	if (flip_h): sprite.flip_h = true
